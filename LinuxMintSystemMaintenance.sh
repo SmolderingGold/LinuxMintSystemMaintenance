885b458e2de52ec7f2c2c4fc84b28b439d5af417
SmolderingGold <applogin@tutamail.com>
2025-11-14 12:01:52 +0000

    LinuxMintSystemMaintenance.sh

---
+++ LinuxMintSystemMaintenance.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "===→ Starting system maintenance ←==="
+
+echo "STEP 1: Attempting to create safety snapshot..."
+
+if ! sudo timeshift --create --tags O --comment "Weekly update $(date +%Y-%m-%d)"; then
+    
+    echo "CRITICAL FAILURE: Timeshift snapshot failed!"
+    echo "Updates stopped! See error above."
+    
+else
+    
+    echo "STEP 1 SUCCESS: Snapshot created. Proceeding to Updates."
+    
+    echo "STEP 2: Refreshing package lists..."
+    sudo apt update
+    
+    echo "STEP 3: Upgrading system packages..."
+    sudo apt upgrade -y
+    
+    echo "STEP 4: Updating Flatpaks..."
+    flatpak update -y
+    
+fi
+
+echo "STEP 5: Running cleanup tasks..."
+
+sudo apt autoremove -y ;
+sudo apt clean ;
+flatpak uninstall --unused -y ;
+
+echo "===→ System maintenance finished ←==="
